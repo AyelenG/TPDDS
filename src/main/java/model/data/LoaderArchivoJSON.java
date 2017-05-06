b89701c843b143db99055526887fdf6b439812e3
carogon <carolina.malen.gonzalez@gmail.com>
2017-05-05 21:14:30 -0300

    utilazamos jackson para hacer mas expresivo la parte de la carga de un archivo json

--- src/main/java/model/data/LoaderArchivoJSON.java
+++ src/main/java/model/data/LoaderArchivoJSON.java
@@ -1,58 +1,28 @@
 package model.data;
-
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
-import model.Cuenta;
 import model.Empresa;
-import model.Periodo;
+import org.codehaus.jackson.map.ObjectMapper;
+
 
 public class LoaderArchivoJSON extends LoaderArchivo {
 
 	public LoaderArchivoJSON(String ruta) {
 		super(ruta);
 	}
-
+	
 	@Override
 	protected List<Empresa> parse(FileReader archivo) throws IOException, ParseException {
-		JSONParser parser = new JSONParser();
-		JSONObject jsonArchivo = (JSONObject) parser.parse(archivo);
-		return parseEmpresas(jsonArchivo);
+				
+		ObjectMapper mapper = new ObjectMapper();
+		ArrayList<Empresa> empresas = mapper.readValue(new BufferedReader(archivo),
+	    mapper.getTypeFactory().constructCollectionType(ArrayList.class, Empresa.class));
+		
+		return empresas;
 	}
-
-	private List<Empresa> parseEmpresas(JSONObject jsonArchivo) {
-
-		List<Empresa> listaEmpresasFinal = new LinkedList<>();
-		JSONArray jsonEmpresas = (JSONArray) jsonArchivo.get("empresas");
-		for (Object e : jsonEmpresas) {
-			Empresa nuevaEmpresa = ManipuladorDeJson.crearEmpresa(e);
-			listaEmpresasFinal.add(nuevaEmpresa);
-			parsePeriodo(e, nuevaEmpresa);
-		}
-		return listaEmpresasFinal;
-	}
-
-	private void parsePeriodo(Object e, Empresa nuevaEmpresa) {
-		JSONArray jsonPeriodos = (JSONArray) ((JSONObject) e).get("periodos");
-		for (Object p : jsonPeriodos) {
-			Periodo nuevoPeriodo = ManipuladorDeJson.crearPeriodo(p);
-			nuevaEmpresa.agregarPeriodo(nuevoPeriodo);
-			parseCuenta(p, nuevoPeriodo);
-		}
-	}
-
-	private void parseCuenta(Object p, Periodo nuevoPeriodo) {
-		JSONArray jsonCuentas = (JSONArray) ((JSONObject) p).get("cuentas");
-		for (Object c : jsonCuentas) {
-			Cuenta nuevaCuenta = ManipuladorDeJson.crearCuenta(c);
-			nuevoPeriodo.agregarCuenta(nuevaCuenta);
-		}
-	}
+		
 }

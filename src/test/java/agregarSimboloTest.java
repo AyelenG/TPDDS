2833a524253479a8b34464dfa1a2bbb5cc7fdd84
carogon <carolina.malen.gonzalez@gmail.com>
2017-05-19 18:48:18 -0300

    Agregamos tests unitarios

--- src/test/java/agregarSimboloTest.java
+++ src/test/java/agregarSimboloTest.java
@@ -4,6 +4,9 @@ import java.util.LinkedList;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import model.Cuenta;
+import model.Indicador;
 import ui.vm.CargaIndicadoresViewModel;
 
 public class agregarSimboloTest {
@@ -24,5 +27,23 @@ public class agregarSimboloTest {
 		viewModel.agregarSimbolo("50");
 		assertEquals(viewModel.getIngresado(),"ABC + 50");
 	}
+	
+	@Test
+	public void verificarFormateoDeIndicador(){
+		Indicador indicador = new Indicador();
+		indicador.setNombre("Hector");
+		viewModel.setIndicadorSeleccionado(indicador);
+		viewModel.ingresarIndicador();
+		assertEquals(viewModel.getIngresado(), "<HECTOR>");
+	} 
+	
+	@Test
+	public void verificarFormateoDeCuenta(){
+		Cuenta cuenta = new Cuenta();
+		cuenta.setNombre("PBI");
+		viewModel.setCuentaSeleccionada(cuenta);
+		viewModel.ingresarCuenta();
+		assertEquals(viewModel.getIngresado(), "[PBI]");
+	} 
 	
 }

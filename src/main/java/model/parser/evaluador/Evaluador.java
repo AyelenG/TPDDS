05ac4c7326a2542a8cce68758a4a02c7be26e490
damianvc31 <damiancohen93@gmail.com>
2017-05-21 23:10:35 -0300

    Evaluador funciona con indicadores dentro de indicadores
    Division por 0 devuelve null(no se toma como indicador aplicable para un
    periodo)

--- src/main/java/model/parser/evaluador/Evaluador.java
+++ src/main/java/model/parser/evaluador/Evaluador.java
@@ -4,112 +4,157 @@ import java.math.BigDecimal;
 
 import model.Cuenta;
 import model.Indicador;
+import model.Indicadores;
 import model.Periodo;
+import model.repositories.Repositorios;
 
 public class Evaluador {
 	/* Evalua un Indicador en un Periodo determinado */
 	public static BigDecimal evaluar(Indicador indicador, Periodo periodo) {
-	    try {
+		try {
+			Indicadores indicadores = Repositorios.indicadoresPredefinidos;
 			String formula = indicador.getFormula();
-	    	return new BigDecimal ( new Object() {
-		        int pos = -1, ch;
-		        void nextChar() {
-		            ch = (++pos < formula.length()) ? formula.charAt(pos) : -1;
-		        }
-	
-		        boolean eat(int charToEat) {
-		            while (ch == ' ') nextChar();
-		            if (ch == charToEat) {
-		                nextChar();
-		                return true;
-		            }
-		            return false;
-		        }
-	
-		        double parse() {
-		            nextChar();
-		            double x = parseExpression();
-		            if (pos < formula.length()) throw new RuntimeException("Unexpected: " + (char)ch);
-		            return x;
-		        }
-	
-		        // Grammar:
-		        // expression = term | expression `+` term | expression `-` term
-		        // term = factor | term `*` factor | term `/` factor
-		        // factor = `+` factor | `-` factor | `(` expression `)`
-		        //        | number | functionName factor | factor `^` factor
-	
-		        double parseExpression() {
-		            double x = parseTerm();
-		            for (;;) {
-		                if      (eat('+')) x += parseTerm(); // addition
-		                else if (eat('-')) x -= parseTerm(); // subtraction
-		                else return x;
-		            }
-		        }
-	
-		        double parseTerm() {
-		            double x = parseFactor();
-		            for (;;) {
-		                if      (eat('*')) x *= parseFactor(); // multiplication
-		                else if (eat('/')) x /= parseFactor(); // division
-		                else return x;
-		            }
-		        }
-	
-		        double parseFactor() {
-		            if (eat('+')) return parseFactor(); // unary plus
-		            if (eat('-')) return -parseFactor(); // unary minus
-	
-		            double x;
-		            int startPos = this.pos;
-		            if (eat('(')) { // parentheses
-		                x = parseExpression();
-		                eat(')');
-		            /* Reemplazo nombre de Cuentas */
-		            } else if (eat('[')) { 
-		                while (ch != ']') nextChar();
-		                String nombre = formula.substring(startPos+1, this.pos);
-		                Cuenta cuenta = new Cuenta(nombre);
-		                if (periodo.existeCuenta(cuenta))
-		                	x = periodo.buscarCuenta(cuenta).getValor().doubleValue();
-		                else throw new RuntimeException("No existe cuenta: " + nombre);	            	            	
-		            	eat(']');
-		            /* Reemplazo nombre de Indicadores */
-		            /* Falta cambiar cuentas por indicadores */
-		            } else if (eat('<')) {
-		                while (ch != '>') nextChar();
-		                String nombre = formula.substring(startPos+1, this.pos);
-		                Cuenta cuenta = new Cuenta(nombre);
-		                if (periodo.existeCuenta(cuenta))
-		                	x = periodo.buscarCuenta(cuenta).getValor().doubleValue();
-		                else throw new RuntimeException("No existe indicador: " + nombre);	            	            	
-		            	eat('>');
-		            } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
-		                while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
-		                x = Double.parseDouble(formula.substring(startPos, this.pos));
-		            } else if (ch >= 'a' && ch <= 'z') { // functions
-		                while (ch >= 'a' && ch <= 'z') nextChar();
-		                String func = formula.substring(startPos, this.pos);
-		                x = parseFactor();
-		                if (func.equals("sqrt")) x = Math.sqrt(x);
-		                else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
-		                else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
-		                else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
-		                else throw new RuntimeException("Unknown function: " + func);
-		            } else {
-		                throw new RuntimeException("Unexpected: " + (char)ch);
-		            }
-	
-		            if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
-	
-		            return x;
-		        }
-		    }.parse() );
-	    }
-	    catch (Exception e) {
-	    	return null;
-	    }
+			
+			double result = (new Object() {
+				int pos = -1, ch;
+
+				void nextChar() {
+					ch = (++pos < formula.length()) ? formula.charAt(pos) : -1;
+				}
+
+				boolean eat(int charToEat) {
+					while (ch == ' ')
+						nextChar();
+					if (ch == charToEat) {
+						nextChar();
+						return true;
+					}
+					return false;
+				}
+
+				double parse() {
+					nextChar();
+					double x = parseExpression();
+					if (pos < formula.length())
+						throw new RuntimeException("Unexpected: " + (char) ch);
+					return x;
+				}
+
+				// Grammar:
+				// expression = term | expression `+` term | expression `-` term
+				// term = factor | term `*` factor | term `/` factor
+				// factor = `+` factor | `-` factor | `(` expression `)`
+				// | number | functionName factor | factor `^` factor |
+				// `[` nombreCuenta `]` | `<` nombreIndicador `>`
+
+				double parseExpression() {
+					double x = parseTerm();
+					for (;;) {
+						if (eat('+'))
+							x += parseTerm(); // addition
+						else if (eat('-'))
+							x -= parseTerm(); // subtraction
+						else
+							return x;
+					}
+				}
+
+				double parseTerm() {
+					double x = parseFactor();
+					for (;;) {
+						if (eat('*'))
+							x *= parseFactor(); // multiplication
+						else if (eat('/'))
+							x /= parseFactor(); // division
+						else
+							return x;
+					}
+				}
+
+				double parseFactor() {
+					if (eat('+'))
+						return parseFactor(); // unary plus
+					if (eat('-'))
+						return -parseFactor(); // unary minus
+
+					double x;
+					int startPos = this.pos;
+					if (eat('(')) { // parentheses
+						x = parseExpression();
+						eat(')');
+						/* Reemplazo nombre de Cuentas */
+					} else if (eat('[')) {
+						while (ch != ']')
+							nextChar();
+						String nombre = formula.substring(startPos + 1, this.pos);
+						x = parseCuenta(nombre);
+						eat(']');
+						/* Reemplazo nombre de Indicadores */
+					} else if (eat('<')) {
+						while (ch != '>')
+							nextChar();
+						String nombre = formula.substring(startPos + 1, this.pos);
+						x = parseIndicador(nombre);
+						eat('>');
+					} else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+						while ((ch >= '0' && ch <= '9') || ch == '.')
+							nextChar();
+						x = Double.parseDouble(formula.substring(startPos, this.pos));
+					} else if (ch >= 'a' && ch <= 'z') { // functions
+						while (ch >= 'a' && ch <= 'z')
+							nextChar();
+						String func = formula.substring(startPos, this.pos);
+						x = parseFactor();
+						if (func.equals("sqrt"))
+							x = Math.sqrt(x);
+						else if (func.equals("sin"))
+							x = Math.sin(Math.toRadians(x));
+						else if (func.equals("cos"))
+							x = Math.cos(Math.toRadians(x));
+						else if (func.equals("tan"))
+							x = Math.tan(Math.toRadians(x));
+						else
+							throw new RuntimeException("Unknown function: " + func);
+					} else {
+						throw new RuntimeException("Unexpected: " + (char) ch);
+					}
+
+					if (eat('^'))
+						x = Math.pow(x, parseFactor()); // exponentiation
+
+					return x;
+				}
+
+				private double parseIndicador(String nombre) {
+					Indicador indicadorInterno = new Indicador(nombre);
+					BigDecimal x;
+					if (indicadores.existeIndicador(indicadorInterno)) {
+						if ((x = Evaluador.evaluar(indicadores.buscarIndicador(indicadorInterno), periodo)) != null)
+							return x.doubleValue();
+						else
+							throw new RuntimeException("No se puede evaluar indicador: " + nombre);
+					} else
+						throw new RuntimeException("No existe indicador: " + nombre);
+				}
+
+				private double parseCuenta(String nombre) {
+					Cuenta cuenta = new Cuenta(nombre);
+					if (periodo.existeCuenta(cuenta))
+						return periodo.buscarCuenta(cuenta).getValor().doubleValue();
+					else
+						throw new RuntimeException("No existe cuenta: " + nombre);
+				}
+			}.parse());
+			
+			try{
+				return new BigDecimal(result);
+			}
+			catch (NumberFormatException e){
+				return null;
+			}
+		} catch (Exception e) {
+			return null;
+		}
 	}
-	
+
 }
--- src/test/java/EvaluadorTest.java
+++ src/test/java/EvaluadorTest.java
@@ -9,10 +9,11 @@ import model.Cuenta;
 import model.Indicador;
 import model.Periodo;
 import model.parser.evaluador.Evaluador;
+import model.repositories.Repositorios;
 
 public class EvaluadorTest {	
 
-	
+	Indicador indicador = new Indicador("PODER","[FDS] * [EBITDA]");
 	Periodo periodo = new Periodo();
 	Cuenta cuenta1 = new Cuenta("EBITDA", new BigDecimal(5));
 	Cuenta cuenta2 = new Cuenta("FDS", new BigDecimal(3));
@@ -21,6 +22,7 @@ public class EvaluadorTest {
 	public void inicio() {
 		periodo.agregarCuenta(cuenta1);
 		periodo.agregarCuenta(cuenta2);
+		Repositorios.indicadoresPredefinidos.agregarIndicador(indicador);
 	}
 	
 	@Test
@@ -28,6 +30,12 @@ public class EvaluadorTest {
 		assertEquals(3, periodo.buscarCuenta(new Cuenta("FDS")).getValor().doubleValue(), 0);
 		assertEquals(5, periodo.buscarCuenta(new Cuenta("EBITDA")).getValor().doubleValue(), 0);
 	}
+	
+	@Test
+	public void evaluacionConIndicadorEnFormula(){
+		assertEquals(20, Evaluador.evaluar(new Indicador("1","<PODER> + 5"), periodo).doubleValue(), 0);
+	}
+	
 	
 	@Test
 	public void evaluacionConUnPeriodoConCuentasDevuelve3(){
@@ -52,6 +60,11 @@ public class EvaluadorTest {
 	@Test
 	public void verificarEvaluacion2(){
 		assertEquals(0, Evaluador.evaluar(new Indicador("6","0/2"), null).doubleValue(), 0);
+	}
+	
+	@Test
+	public void nullSiDividePor0(){
+		assertNull(Evaluador.evaluar(new Indicador("7","5/0"), null));
 	}
 	
 	@Test

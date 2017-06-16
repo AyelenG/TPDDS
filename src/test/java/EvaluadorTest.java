8b714543d8019d38d4bceede6ae7ac6ca065679d
damianvc31 <damiancohen93@gmail.com>
2017-06-16 18:36:20 -0300

    EvaluadorTest con Theory

--- src/test/java/EvaluadorTest.java
+++ src/test/java/EvaluadorTest.java
@@ -1,17 +1,18 @@
 import static org.junit.Assert.*;
-
-import java.math.BigDecimal;
-
+import org.junit.runner.RunWith;
+import org.junit.experimental.theories.*;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.math.BigDecimal;
 
 import exceptions.NoSePuedeEvaluarException;
 import model.Cuenta;
 import model.Indicador;
 import model.Indicadores;
 import model.Periodo;
-import model.parser.evaluador.Evaluador;
 
+@RunWith(Theories.class)
 public class EvaluadorTest {
 
 	Indicador indicador1 = new Indicador("PODER", "[FDS] * [EBITDA]");
@@ -29,55 +30,49 @@ public class EvaluadorTest {
 		indicadores.agregarElemento(indicador2);
 	}
 
+	static class Pair {
+		Indicador ind;
+		double valor;
+
+		Pair(Indicador ind, double valor) {
+			this.ind = ind;
+			this.valor = valor;
+		}
+	}
+
+	@DataPoints
+	public static Pair[] pairs = { 
+			new Pair(new Indicador("1", "<PODER> + 5"), 20),
+			new Pair(new Indicador("2", "([FDS]+[EBITDA]+1)/3"), 3),
+			new Pair(new Indicador("3", "5+2-3"), 4),
+			new Pair(new Indicador("4", "0/2"), 0)
+	};
+
 	@Test
 	public void compruebaValoresDeCuentas() {
 		assertEquals(3, periodo.buscarCuenta(new Cuenta("FDS")).getValor().doubleValue(), 0);
 		assertEquals(5, periodo.buscarCuenta(new Cuenta("EBITDA")).getValor().doubleValue(), 0);
 	}
 
-	@Test
-	public void evaluacionConIndicadorEnFormula() {
-		assertEquals(20, new Indicador("1", "<PODER> + 5").evaluar(periodo, indicadores).doubleValue(), 0);
-	}
-
-	@Test
-	public void evaluacionConUnPeriodoConCuentasDevuelve3() {
-		assertEquals(3, new Indicador("2", "([FDS]+[EBITDA]+1)/3").evaluar(periodo, indicadores).doubleValue(), 0);
-	}
-
 	@Test(expected = NoSePuedeEvaluarException.class)
 	public void mensajeDeErrorSiCuentaNoEstaEnPeriodo() {
-		new Indicador("3", "[FDS]+[NO ESTA]+1").evaluar(periodo, indicadores);
+		new Indicador("5", "[FDS]+[NO ESTA]+1").evaluar(periodo, indicadores);
 	}
 
 	@Test(expected = NoSePuedeEvaluarException.class)
 	public void mensajeDeErrorSiIndicadorInternoNoSePuedeCalcular() {
-		new Indicador("4", "8 * <INCALCULABLE>").evaluar(periodo, indicadores);
+		new Indicador("6", "8 * <INCALCULABLE>").evaluar(periodo, indicadores);
 	}
 
-	@Test
-	public void verificarEvaluacion() {
-		assertEquals(4, new Indicador("5", "5+2-3").evaluar(null, indicadores).doubleValue(), 0);
-	}
-
-	@Test
-	public void verificarEvaluacion1() {
-		assertEquals(6.5, Evaluador.evaluar(new Indicador("6", "(5+3/2)"), null, indicadores).doubleValue(), 0);
-	}
-
-	@Test
-	public void verificarEvaluacion2() {
-		assertEquals(0, new Indicador("7", "0/2").evaluar(null, indicadores).doubleValue(), 0);
+	@Theory
+	public void verificarEvaluacion(Pair pair) {
+		assertEquals(pair.valor, pair.ind.evaluar(periodo, indicadores).doubleValue(),0);
 	}
 
 	@Test(expected = NoSePuedeEvaluarException.class)
 	public void mensajeDeErrorSiDividePor0() {
-		new Indicador("8", "5/0").evaluar(null, indicadores);
+		new Indicador("7", "5/0").evaluar(null, indicadores);
 	}
 
-	@Test
-	public void verificarFuncionCosenoCeroGradosDa1() {
-		assertEquals(1, new Indicador("9", "cos0").evaluar(null, indicadores).doubleValue(), 0);
-	}
 
 }

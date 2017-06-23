0c9b680a313b66a2089143281d3eaa89ac8e1dd1
carogon <carolina.malen.gonzalez@gmail.com>
2017-06-23 12:57:13 -0300

    test para sumar valores de las listas

---
+++ src/main/java/model/Metodología.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.util.List;
+
+public class Metodología {
+
+	
+	public boolean maximizarROE (Empresa empresa1, Empresa empresa2)
+	{
+		/*List <Periodo> periodos1 = empresa1.ultimosNPeriodos(10);
+		List <Periodo> periodos2 =empresa2.ultimosNPeriodos(10);
+		Cuenta ROE = new Cuenta("ROE");
+		
+		
+		boolean uno = periodos1.stream().allMatch(periodo -> periodo.existeCuenta(ROE));
+		boolean dos = periodos2.stream().allMatch(periodo -> periodo.existeCuenta(ROE));
+		
+		if ( uno && dos)
+		{
+			periodos1.stream().sum(periodo -> periodo.getValor(ROE));
+		}*/
+		return true;
+		
+		
+	}
+	
+}
---
+++ src/test/java/Listas/SumatoriaTest.java
@@ -0,0 +1,47 @@
+package Listas;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.runner.RunWith;
+
+import model.Cuenta;
+import model.Periodo;
+
+import org.junit.experimental.theories.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+@RunWith(Theories.class)
+public class SumatoriaTest {
+	
+	@Test
+	public void sumatoriaDeLista(){		
+		List <BigDecimal> lista = Arrays.asList(new BigDecimal(5), new BigDecimal(5),new BigDecimal(5));
+		int sumatoria = lista.stream().mapToInt(bigDecimal -> bigDecimal.intValue()).sum();
+		Assert.assertEquals(15, sumatoria);
+
+	}
+	
+	@Test
+	public void sumatoriaDeListaDePeriodos(){
+	
+	Periodo periodo1 = new Periodo();
+	Periodo periodo2 = new Periodo();
+	Periodo periodo3 = new Periodo();
+	Cuenta cuenta = new Cuenta("ROE", new BigDecimal(5));
+	periodo1.agregarCuenta(cuenta);
+	periodo2.agregarCuenta(cuenta);
+	periodo3.agregarCuenta(cuenta);
+	List<Periodo> lista = Arrays.asList(periodo1,periodo2,periodo3);
+	
+	
+	int sumatoria = lista.stream().mapToInt(periodo -> periodo.buscarCuenta(cuenta).getValor().intValue()).sum();
+	Assert.assertEquals (15, sumatoria);
+	}
+}

ba81dac4a8ba15c0cd215ed53459d7566957183b
carogon <carolina.malen.gonzalez@gmail.com>
2017-07-04 15:33:29 -0300

    vista no taxativa sin comportamiento

--- src/main/java/ui/vm/metodologia/NuevaCondicionNoTaxativaViewModel.java
+++ src/main/java/ui/vm/metodologia/NuevaCondicionNoTaxativaViewModel.java
@@ -1,21 +1,30 @@
 package ui.vm.metodologia;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.uqbar.commons.model.ObservableUtils;
 import org.uqbar.commons.utils.Observable;
 
+import model.Indicador;
 import model.condiciones.Mayor;
 import model.condiciones.Menor;
 import model.condiciones.notaxativas.CondicionNoTaxativaConfigurable;
 import model.condiciones.taxativas.CondicionTaxativaConfigurable;
 import model.condiciones.taxativas.Simple;
+import model.repositories.RepoIndicadores;
 
 @Observable
 public class NuevaCondicionNoTaxativaViewModel {
 
 	private CargaMetodologiaViewModel parentVM;
 	private CondicionNoTaxativaConfigurable condNoTaxativa;
+	private Integer peso;
+	private RepoIndicadores indicadores = RepoIndicadores.getInstance();
+	private Indicador indicadorSeleccionado;
+	private List <String> comparadores;
+	private String comparadorSeleccionado;
+	private Integer anios;
 	
 	public NuevaCondicionNoTaxativaViewModel(CargaMetodologiaViewModel _parentVM) {
 			this.parentVM = _parentVM;
@@ -39,6 +48,54 @@ public class NuevaCondicionNoTaxativaViewModel {
 
 	public void setCondNoTaxativa(CondicionNoTaxativaConfigurable condNoTaxativa) {
 		this.condNoTaxativa = condNoTaxativa;
+	}
+
+	public Integer getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Integer peso) {
+		this.peso = peso;
+	}
+
+	public List<Indicador> getIndicadores() {
+		return indicadores.getElementos();
+	}
+
+	public void setIndicadores(RepoIndicadores indicadores) {
+		this.indicadores = indicadores;
+	}
+
+	public Indicador getIndicadorSeleccionado() {
+		return indicadorSeleccionado;
+	}
+
+	public void setIndicadorSeleccionado(Indicador indicadorSeleccionado) {
+		this.indicadorSeleccionado = indicadorSeleccionado;
+	}
+
+	public List<String> getComparadores() {
+		return comparadores;
+	}
+
+	public void setComparadores(List<String> comparadores) {
+		this.comparadores = comparadores;
+	}
+
+	public String getComparadorSeleccionado() {
+		return comparadorSeleccionado;
+	}
+
+	public void setComparadorSeleccionado(String comparadorSeleccionado) {
+		this.comparadorSeleccionado = comparadorSeleccionado;
+	}
+
+	public Integer getAnios() {
+		return anios;
+	}
+
+	public void setAnios(Integer anios) {
+		this.anios = anios;
 	}
 	
 	
--- src/main/java/ui/windows/metodologia/NuevaCondicionNoTaxativaWindow.java
+++ src/main/java/ui/windows/metodologia/NuevaCondicionNoTaxativaWindow.java
@@ -5,10 +5,12 @@ import org.uqbar.arena.layout.VerticalLayout;
 import org.uqbar.arena.widgets.Button;
 import org.uqbar.arena.widgets.Label;
 import org.uqbar.arena.widgets.Panel;
+import org.uqbar.arena.widgets.Selector;
 import org.uqbar.arena.widgets.TextBox;
 import org.uqbar.arena.windows.Window;
 import org.uqbar.arena.windows.WindowOwner;
 
+import model.Indicador;
 import ui.vm.metodologia.CargaMetodologiaViewModel;
 import ui.vm.metodologia.NuevaCondicionNoTaxativaViewModel;
 
@@ -37,5 +39,37 @@ public class NuevaCondicionNoTaxativaWindow  extends Window<NuevaCondicionNoTaxa
 			new Label(nombrePanel).setText("Nombre de la condición  ");
 			TextBox nombre = new TextBox(nombrePanel);
 			nombre.bindValueToProperty("condNoTaxativa.nombre");
+			
+			new Label(nombrePanel).setText("Peso");
+			TextBox peso = new TextBox(nombrePanel);
+			peso.bindValueToProperty("peso");
+			
+			// Indicadores
+
+			Panel comparadorPanel = new Panel(mainPanel);
+			comparadorPanel.setLayout(new HorizontalLayout());
+					
+				Panel indicadorPanel = new Panel(comparadorPanel);
+				indicadorPanel.setLayout(new VerticalLayout());
+
+				new Label(indicadorPanel).setText("Indicadores");
+				Selector<Indicador> selectorIndicador = new Selector<Indicador>(indicadorPanel).allowNull(true);
+				selectorIndicador.bindItemsToProperty("indicadores");
+				selectorIndicador.bindValueToProperty("indicadorSeleccionado");
+					
+				Panel operadorPanel = new Panel(comparadorPanel);
+				operadorPanel.setLayout(new VerticalLayout());
+
+				new Label(operadorPanel).setText("Comparador");
+				Selector<String> selectorOperador = new Selector<String>(operadorPanel).allowNull(true);
+				selectorOperador.bindItemsToProperty("comparadores");
+				selectorOperador.bindValueToProperty("comparadorSeleccionado");
+			
+				Panel anioPanel = new Panel(mainPanel);
+				anioPanel.setLayout(new VerticalLayout());
+				
+				new Label(anioPanel).setText("Años a aplicar contando desde el último");
+				TextBox anios = new TextBox(anioPanel);
+				anios.bindValueToProperty("anios");
 	}
 }

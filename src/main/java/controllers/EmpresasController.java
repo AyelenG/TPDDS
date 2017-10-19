618a1232c7e2f6c274b92643c9df3409c791b022
ayelenG <ayelen.guimarey@hotmail.com>
2017-10-19 02:43:13 -0300

    cuentas

--- src/main/java/controllers/EmpresasController.java
+++ src/main/java/controllers/EmpresasController.java
@@ -4,13 +4,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import model.Cuenta;
 import model.CuentaPeriodo;
 import model.Empresa;
-import model.Metodologia;
+
 import model.Periodo;
 import model.repositories.RepoEmpresas;
-import model.repositories.RepoMetodologias;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -24,19 +22,21 @@ public class EmpresasController {
 	}
 	public static ModelAndView handleSeleccionPeriodo(Request req, Response res){
 		Map<String, List<Periodo>> model = new HashMap<>();
+		String periodo = req.queryParams("periodo");
 		Empresa empresa = RepoEmpresas.getInstance().get(Long.parseLong(req.params("empresa")));
 		
-		
+		if(periodo!=null){
+			res.redirect(req.pathInfo() +"/" + periodo);
+		}
 		model.put("periodos", empresa.getPeriodos());		
 		return new ModelAndView(model, "empresa/selector-periodo.hbs");
 		
 	}
 	public static ModelAndView handleCuentas(Request req, Response res) {
 		Map<String, List<CuentaPeriodo>> model = new HashMap<>();
-		Empresa empresaElegida = RepoEmpresas.getInstance().get(Long.parseLong(req.params("empresa")));
-		
+		Empresa empresa = RepoEmpresas.getInstance().get(Long.parseLong(req.params("empresa")));
 		Integer anio = Integer.valueOf(req.params("periodo"));
-		Periodo periodo = empresaElegida.buscarPeriodo(new Periodo(anio));
+		Periodo periodo = empresa.buscarPeriodo(new Periodo(anio));
 		List<CuentaPeriodo> cuentas = periodo.getCuentas();
 		
 		model.put("cuentas", cuentas);
